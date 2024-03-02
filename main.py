7e59fa8354f60edf424dea3371f5abc8a3fcebab
Ihor <skogrv@gmail.com>
2024-03-02 22:42:38 +0200

    added get request

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+class Hey(Resource):
+    def get(self):
+        return {"data": "hey"}
+    
+api.add_resource(Hey, "/hey")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
